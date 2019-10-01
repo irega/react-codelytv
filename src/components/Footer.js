a314683ce5d84af516b4a170c14f1f0f13e8ebdb
irega <ivan.reinoso@outlook.es>
2019-10-01 16:42:39 +0200

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+    <div className="footer-content">
+        <div className="footer-title-text" >Created by: irega</div>
+    </div>
+);
+
+export default Footer;
